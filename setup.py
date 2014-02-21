dafc58a0f8b1e9c03550e0313c1fdcc3c4d633fc
Ori Hoch <ori@uumpa.com>
2014-02-21 11:46:43 +0200

    v0.0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='okscraper-django',
-    version='0.1',
+    version='0.0.2',
     description='okscraper django integration',
     author='Ori Hoch',
     author_email='ori@uumpa.com',
     license='GPLv3',
     url='https://github.com/orihoch/okscraper-django',
     packages=['okscraper_django'],
-    install_requires=['okscraper==0.0.1'],
-    dependency_links=['git+ssh://git@github.com/hasadna/okscraper.git@v0.0.1#egg=okscraper-0.0.1']
+    install_requires=['okscraper==0.0.2'],
+    dependency_links=['git+ssh://git@github.com/OriHoch/okscraper.git@v0.0.2#egg=okscraper-0.0.2']
 )

fdc7901a844e0f14f564b0c1f2db7743b7ecf856
Ori Hoch <ori@uumpa.com>
2014-02-22 23:54:23 +0200

    fix setup.py to find packages and bump okscraper to 0.0.3

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='okscraper-django',
@@ -8,7 +8,7 @@ setup(
     author_email='ori@uumpa.com',
     license='GPLv3',
     url='https://github.com/orihoch/okscraper-django',
-    packages=['okscraper_django'],
-    install_requires=['okscraper==0.0.2'],
-    dependency_links=['https://github.com/hasadna/okscraper/archive/v0.0.2.tar.gz#egg=okscraper-0.0.2']
+    packages=find_packages(exclude=["tests", "test.*"]),
+    install_requires=['okscraper==0.0.3'],
+    dependency_links=['https://github.com/hasadna/okscraper/archive/v0.0.3.tar.gz#egg=okscraper-0.0.3']
 )

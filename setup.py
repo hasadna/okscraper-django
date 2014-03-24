344cf64b7174d48e7edbcb2b30b15f1c954bca44
Ori Hoch <ori@uumpa.com>
2014-03-24 20:58:41 +0200

    v0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='okscraper-django',
-    version='0.0.7',
+    version='0.0.8',
     description='okscraper django integration',
     author='Ori Hoch',
     author_email='ori@uumpa.com',
